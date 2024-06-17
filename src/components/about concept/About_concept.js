8cba7c60c0f15abbbf106b05472f75b20222d9a8
Sougata Mukherjee <tmjit97@gmail.com>
2024-06-17 12:21:15 +0530

    changes about

--- src/components/about concept/About_concept.js
+++ src/components/about concept/About_concept.js
@@ -12,20 +12,19 @@ export default function About_concept() {
               <h2>What we do</h2>
               <p>
                 G Lo Dobaara" (popularly known as “GLD”) is a distinct Strategic
-                Business Unit (SBU) of Abchi Technologies Private Limited and an
-                initiative of social-entrepreneurship. GLD provides quality time
-                to elderly people who need a companion. High-achieving children
-                now relocate on their own to a new city or perhaps another
-                country for a career or married life. Families in metropolitan
-                cultures are getting estranged from their own neighbourhoods. In
-                addition, the majority of metropolitan households only have one
-                child. If their children relocate to a new city, the elderly
-                couples feel awkward to cope up in a different culture and
-                rather feel comfortable staying at their original residences.
-                Their main problem is loneliness and it is taking a toll on
-                their mental & physical health. Here, the couples and their
-                children are helpless because they don’t know who can take them
-                out of this situation...
+                Business Unit (SBU) of Abchi Infrastructure Private Limited and
+                an initiative of social-entrepreneurship. GLD provides quality
+                time to elderly people who need a companion. High-achieving
+                children now relocate on their own to a new city or perhaps
+                another country for a career or married life. Families in
+                metropolitan cultures are getting estranged from their own
+                neighbourhoods. In addition, the majority of metropolitan
+                households only have one child. If their children relocate to a
+                new city, the elderly couples feel awkward to cope up in a
+                different culture and rather feel comfortable staying at their
+                original residences. Their main problem is loneliness and it is
+                taking a toll on their mental & physical health. Here, the
+                couples and their children are helpless because they don’t...
               </p>
               <a href="/about">
                 <span>Read More</span>
@@ -42,17 +41,17 @@ export default function About_concept() {
             <div className="box-left box-left-spcl">
               <h2>About</h2>
               <p>
-                "G Lo Dobaara" (popularly known as “GLD”) is a distinct
-                Strategic Business Unit (SBU) of Abchi Technologies Private
-                Limited and an initiative of social entrepreneurship. Abchi
-                Technologies Private Limited (popularly known as “Abchi”) is an
-                innovative IT Company that predominantly works to develop brands
-                (IT Products) for different kinds of services. Abchi is slowly
-                building itself as a visionary and a thought leader in the field
-                of IT and a pioneer of Gen-Next IT Products/Services. Moreover,
-                Abchi wants to contribute something to change every living
-                being’s life for the better, however small that contribution may
-                be...
+                G Lo Dobaara" (popularly known as “GLD”) is a distinct Strategic
+                Business Unit (SBU) of Abchi Infrastructure Private Limited and
+                an initiative of social entrepreneurship. Abchi Infrastructure
+                Private Limited (popularly known as “Abchi”) is an innovative
+                solution providing Company that predominantly works to develop
+                Infrastructure based products (IT Products) for solving
+                different kinds of problems in society. Abchi is slowly building
+                itself as a visionary and a thought leader in the field of IT
+                and a pioneer of Gen-Next IT Products/Services. Moreover, Abchi
+                wants to contribute something to change every living being’s
+                life for the better, however small that contribution may be...
               </p>
               <a href="/about">
                 <span>Know More</span>
